353a9c6dd9aff403be2e6a699f16f746dfa53c2e
Jameka <meka.jay@live.com>
2017-07-05 12:26:22 -0500

    liri

---
+++ keys.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '5Zyc4Q8yOGmi0BwFNCmc1qKUd',
+  consumer_secret: 'L9ZY5FnwRTIIJqEr8PNtF8wbNbNdfURnOexb1WJHDAu6FVDKws',
+  access_token_key: '881276337620738049-wOWS6Kaxm7Sdp5XogScWKz6gVNsqgoq',
+  access_token_secret: 'J63cFdoQ75TLtcbSuPnLq7dVk4p4lymxvq51QghVJpJtg',
+};
